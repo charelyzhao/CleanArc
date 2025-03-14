79aa3eaeddbd7cdedb166febf5ec868096e27d1e
czhao <czhao@nuvasive.com>
2025-03-14 20:09:53 +0800

    初始

---
+++ CleanArc/src/Domain/Common/BaseEvent.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CleanArc.Domain.Common;
+
+public abstract class BaseEvent : INotification
+{
+}
---
+++ CleanArc/tests/Application.FunctionalTests/BaseTestFixture.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArc.Application.FunctionalTests;
+
+using static Testing;
+
+[TestFixture]
+public abstract class BaseTestFixture
+{
+    [SetUp]
+    public async Task TestSetUp()
+    {
+        await ResetState();
+    }
+}
