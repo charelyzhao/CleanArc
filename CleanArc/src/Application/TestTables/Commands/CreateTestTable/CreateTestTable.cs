394618199d48233d701f012ee21a14a54238cbeb
czhao <czhao@nuvasive.com>
2025-03-14 21:56:03 +0800

    3

---
+++ CleanArc/src/Application/TestTables/Commands/CreateTestTable/CreateTestTable.cs
@@ -0,0 +1,38 @@
+﻿using CleanArc.Application.Common.Interfaces;
+using CleanArc.Domain.Entities;
+
+namespace CleanArc.Application.TestTables.Commands.CreateTestTable;
+
+public record CreateTestTableCommand : IRequest<int>
+{
+    public string? Name { get; set; }
+}
+
+public class CreateTestTableCommandValidator : AbstractValidator<CreateTestTableCommand>
+{
+    public CreateTestTableCommandValidator()
+    {
+    }
+}
+
+public class CreateTestTableCommandHandler : IRequestHandler<CreateTestTableCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateTestTableCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateTestTableCommand request, CancellationToken cancellationToken)
+    {
+        var entitys = new TestTable
+        {
+            Name = request.Name
+        };
+        _context.TestTables.Add(entitys);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entitys.Id;
+    }
+}
---
+++ CleanArc/src/Web/Endpoints/TestTables.cs
@@ -0,0 +1,24 @@
+﻿using CleanArc.Application.Common.Models;
+using CleanArc.Application.TestTables.Commands.CreateTestTable;
+using CleanArc.Application.TodoItems.Commands.CreateTodoItem;
+using CleanArc.Application.TodoItems.Commands.DeleteTodoItem;
+using CleanArc.Application.TodoItems.Commands.UpdateTodoItem;
+using CleanArc.Application.TodoItems.Commands.UpdateTodoItemDetail;
+using CleanArc.Application.TodoItems.Queries.GetTodoItemsWithPagination;
+
+namespace CleanArc.Web.Endpoints;
+
+public class TestTables : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .MapPost(CreateTestTable);
+    }
+    public Task<int> CreateTestTable(ISender sender, CreateTestTableCommand command)
+    {
+        return sender.Send(command);
+    }
+
+   
+}
